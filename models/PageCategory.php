bd12461d0c00304145aadbd10d1b20a436ed66f2
Anderson Danilo <contato@andersondanilo.com>
2014-11-04 22:41:08 -0200

    localized pages

--- models/PageCategory.php
+++ models/PageCategory.php
@@ -4,7 +4,7 @@ namespace Tee\Page\Models;
 
 use Illuminate\Auth\UserInterface;
 use Illuminate\Database\Eloquent\SoftDeletingTrait;
-use Eloquent;
+use Eloquent, Config;
 
 class PageCategory extends Eloquent {
 
@@ -14,6 +14,19 @@ class PageCategory extends Eloquent {
 
     public function pages() {
         return $this->hasMany(__NAMESPACE__.'\\Page')->orderBy('order');
+    }
+
+    public function localizedPages() {
+        $queryBuilder = $this->pages();
+        if(moduleEnabled('i18n'))
+        {
+            $queryBuilder->where(function($query) {
+                $query->where('language', '=', Config::get('app.locale'))
+                    ->orWhere('language', '=', '')
+                    ->orWhereNull('language');
+            });
+        }
+        return $queryBuilder;
     }
 
 }
